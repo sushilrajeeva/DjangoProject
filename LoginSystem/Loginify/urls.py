355ec229cd5123ba9c64bdb450a6e2a546c264f6
Sushil Rajeeva Bhandary <sushilrajeeva@gmail.com>
2025-11-19 06:43:23 -0500

    feat: implement signup/login views and templates

--- LoginSystem/Loginify/urls.py
+++ LoginSystem/Loginify/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.hello_world, name='hello-world'),
+    path('signup/', views.signup_view, name='signup'),
+    path('login/', views.login_view, name='login'),
 ]
