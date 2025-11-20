18d43bfc82acb658ecf72adb43a4659db84b6d8d
Sushil Rajeeva Bhandary <sushilrajeeva@gmail.com>
2025-11-20 06:46:52 -0500

    update bug fix

--- LoginSystem/Loginify/urls.py
+++ LoginSystem/Loginify/urls.py
@@ -5,4 +5,8 @@ urlpatterns = [
     path('', views.hello_world, name='hello-world'),
     path('signup/', views.signup_view, name='signup'),
     path('login/', views.login_view, name='login'),
+    path('api/users/', views.get_all_users, name='api-get-all-users'),
+    path('api/users/<str:email>/', views.get_user_by_email, name='api-get-user'),
+    path('api/users/<str:email>/update/', views.update_user, name='api-update-user'),
+    path('api/users/<str:email>/delete/', views.delete_user, name='api-delete-user'),
 ]
